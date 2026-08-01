f4c926688a983f1705e3d2fce781104546a92758
VornMakara <vornmakara0201@gmail.com>
2026-08-01 23:01:12 +0700

    test: testing pull request CI workflow

--- tests/Feature/AdminCrudTest.php
+++ tests/Feature/AdminCrudTest.php
@@ -26,7 +26,7 @@ class AdminCrudTest extends TestCase
     public function test_admin_can_create_category()
     {
         $adminRole = Role::where('name', 'Admin')->first();
-        
+        //
         $branch = Branch::create([
             'name' => 'Main Branch ' . uniqid(),
         ]);
