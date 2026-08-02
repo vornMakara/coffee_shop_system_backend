3ddcb426404bd100bebad8d8a425b6bbd0f041bf
VornMakara <vornmakara0201@gmail.com>
2026-08-02 22:09:21 +0700

    Update welcome page title to test CI/CD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <title>Laravel - Coffee Shop</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
